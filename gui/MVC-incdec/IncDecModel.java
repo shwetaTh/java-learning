c91703929a539ed9f19c324bd5e1954b52ad673b
shweta <121001392+shwetaTh@users.noreply.github.com>
2024-06-06 21:00:01 +0545

    model portion of the MVC inc dec counter

---
+++ gui/MVC-incdec/IncDecModel.java
@@ -0,0 +1,11 @@
+public class IncDecModel {
+    private int num = 0;
+
+    public int getNum() {
+        return num;
+    }
+
+    public void setNum(int num) {
+        this.num = num;
+    }
+}
