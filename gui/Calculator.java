bccf00040e3f17a375a9af894a8de510d96748b5
shweta <121001392+shwetaTh@users.noreply.github.com>
2024-06-14 19:16:44 +0545

    Added buttons and action listener to all btns

--- gui/Calculator.java
+++ gui/Calculator.java
@@ -5,16 +5,53 @@ public class Calculator {
     public static void main(String[] args) {
         JFrame frame = new JFrame("Calculator");
         frame.setLayout(new GridLayout(2, 1));
-        JPanel input = new JPanel();
+        JTextField display = new JTextField();
         JPanel nums= new JPanel();
         nums.setLayout(new GridLayout(4, 4));
         nums.setBorder(BorderFactory.createLineBorder(Color.BLACK));
-        frame.add(input);
+        frame.add(display);
         frame.add(nums);
-        JButton[] buttons = new JButton[16];
-        for (int i = 0; i<16; i++){
-            buttons[i]= new JButton("X");
-            nums.add(buttons[i]);
+        JButton cAdd = new JButton("+");
+        nums.add(cAdd);
+        JButton cSub = new JButton("-");
+        nums.add(cSub);
+        JButton cX = new JButton("X");
+        nums.add(cX);
+        JButton cClear = new JButton("C");
+        nums.add(cClear);
+
+        JButton c1 = new JButton("1");
+        nums.add(c1);
+        JButton c2 = new JButton("2");
+        nums.add(c2);
+        JButton c3 = new JButton("3");
+        nums.add(c3);
+        JButton cSqr = new JButton("Sqrt");
+        nums.add(cSqr);
+
+        JButton c4 = new JButton("4");
+        nums.add(c4);
+        JButton c5 = new JButton("5");
+        nums.add(c5);
+        JButton c6 = new JButton("6");
+        nums.add(c6);
+        JButton cDiv = new JButton("/");
+        nums.add(cDiv);
+
+        JButton c7 = new JButton("7");
+        nums.add(c7);
+        JButton c8 = new JButton("8");
+        nums.add(c8);
+        JButton c9 = new JButton("9");
+        nums.add(c9);
+        JButton cEqls = new JButton("=");
+        nums.add(cEqls);
+
+        Component[] components = nums.getComponents();
+        for (Component component : components) {
+            if (component instanceof JButton) {
+                ((JButton) component).addActionListener(new BtnClick());
+            }
         }
         frame.setSize(500, 400);
         frame.setVisible(true);
