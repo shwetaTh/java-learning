a6420ec2528945b6d5bcf87f2f40228d1231c090
shweta <121001392+shwetaTh@users.noreply.github.com>
2024-07-01 19:17:28 +0545

    added user input

--- NetworkProgramming/LabWorks/Unit3/Lab3.java
+++ NetworkProgramming/LabWorks/Unit3/Lab3.java
@@ -1,30 +1,38 @@
 package LabWorks.Unit3;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLConnection;
+import java.net.*;
+import java.util.Scanner;
 
 public class Lab3 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String[] exampleUrls = {
+                "https://www.example.com/path/to/resource?query=value#fragment",
+                "http://java.sun.com/j2se/1.3/",
+                "ftp://ftp.is.co.za/rfc/rfc1808.txt"
+        };
+        System.out.print("Enter a URL: ");
+        String userInput = scanner.nextLine();
+        analyzeURL(userInput);
+        scanner.close();
+    }
+    public static void analyzeURL(String urlString) {
         try {
+            URL url = new URL(urlString);
 
-            //URL into pieces
-
-            URL urlPieces = new URL("http://www.ibiblio.org/javafaq/books/jnp/index.html?isbn=1565922069#toc,");
-            System.out.println("Protocol: " + urlPieces.getProtocol());
-            System.out.println("Host: " + urlPieces.getHost());
-            System.out.println("Port: " + urlPieces.getPort());
-            System.out.println("File: " + urlPieces.getFile());
-            System.out.println("Ref: " + urlPieces.getRef()); //returns fragment
-            System.out.println("Query: " + urlPieces.getQuery());
-            System.out.println("Path: " + urlPieces.getPath());
-            System.out.println("UserInfo: " + urlPieces.getUserInfo());
-            System.out.println("Authority: " + urlPieces.getAuthority());
+            System.out.println("Protocol: " + url.getProtocol());
+            System.out.println("Host: " + url.getHost());
+            System.out.println("Port: " + url.getPort());
+            System.out.println("File: " + url.getFile());
+            System.out.println("Ref: " + url.getRef()); // returns fragment
+            System.out.println("Query: " + url.getQuery());
+            System.out.println("Path: " + url.getPath());
+            System.out.println("UserInfo: " + url.getUserInfo());
+            System.out.println("Authority: " + url.getAuthority());
 
         } catch (MalformedURLException e) {
-            throw new RuntimeException(e);
+            System.err.println("Invalid URL: " + urlString);
+            e.printStackTrace();
         }
     }
 }
