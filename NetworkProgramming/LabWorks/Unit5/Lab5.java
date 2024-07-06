7f1d086f99f059603cf44fcf172482a0b46dfb3a
shweta <121001392+shwetaTh@users.noreply.github.com>
2024-07-06 16:26:10 +0545

    http request handling code

--- NetworkProgramming/LabWorks/Unit5/Lab5.java
+++ NetworkProgramming/LabWorks/Unit5/Lab5.java
@@ -1,56 +1,41 @@
 package LabWorks.Unit5;
 
 import java.io.IOException;
+import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
-import java.io.OutputStream;
 public class Lab5 {
-    public static HttpURLConnection sendGetRequest(String urlString) throws IOException {
-        URL url = new URL(urlString);
-        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-        connection.setRequestMethod("GET");
-        return connection;
-    }
-    public static HttpURLConnection sendPostRequest(String urlString, String payload) throws IOException {
-        URL url = new URL(urlString);
-        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-        connection.setRequestMethod("POST");
-        connection.setDoOutput(true);
-        connection.setRequestProperty("Content-Type", "application/json");
-        try (OutputStream os = connection.getOutputStream()) {
-            byte[] input = payload.getBytes("utf-8");
-            os.write(input, 0, input.length);
-        }
-        return connection;
-    }
-    public static HttpURLConnection sendTraceRequest(String urlString) throws IOException {
-        URL url = new URL(urlString);
-        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-        connection.setRequestMethod("TRACE");
-        return connection;
-    }
-    public static HttpURLConnection sendOptionsRequest(String urlString) throws IOException {
-        URL url = new URL(urlString);
-        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-        connection.setRequestMethod("OPTIONS");
-        return connection;
-    }
     public static void main(String[] args) {
         try {
             String getUrl = "http://example.com";
             String postUrl = "http://example.com";
             String postPayload = "{\"key\":\"value\"}";
 
-            HttpURLConnection getResponse = sendGetRequest(getUrl);
-            HttpURLConnection postResponse = sendPostRequest(postUrl, postPayload);
-            HttpURLConnection traceResponse = sendTraceRequest(getUrl);
-            HttpURLConnection optionsResponse = sendOptionsRequest(getUrl);
-
+            // Sending HTTP GET request
+            HttpURLConnection getResponse = (HttpURLConnection) new URL(getUrl).openConnection();
+            getResponse.setRequestMethod("GET");
             System.out.println("GET Request Response Code: " + getResponse.getResponseCode());
-            System.out.println("POST Request Response Code: " + postResponse.getResponseCode());
-            System.out.println("TRACE Request Response Code: " + traceResponse.getResponseCode());
-            System.out.println("OPTIONS Request Response Code: " + optionsResponse.getResponseCode());
+
+            // Sending HTTP POST request
+            HttpURLConnection postResponse = (HttpURLConnection) new URL(postUrl).openConnection();
+            postResponse.setRequestMethod("POST");
+            postResponse.setDoOutput(true);
+            try (OutputStream os = postResponse.getOutputStream()) {
+                byte[] input = postPayload.getBytes("utf-8");
+                os.write(input, 0, input.length);
+            }
+            System.out.println("POST response code: " + postResponse.getResponseCode());
+
+            // Sending HTTP TRACE request
+            HttpURLConnection traceResponse = (HttpURLConnection) new URL(getUrl).openConnection();
+            traceResponse.setRequestMethod("TRACE");
+            System.out.println("TRACE response code: " + traceResponse.getResponseCode());
+
+            // Sending HTTP OPTIONS request
+            HttpURLConnection optionsResponse = (HttpURLConnection) new URL(getUrl).openConnection();
+            optionsResponse.setRequestMethod("OPTIONS");
+            System.out.println("OPTIONS response code: " + optionsResponse.getResponseCode());
 
         } catch (IOException e) {
             e.printStackTrace();
