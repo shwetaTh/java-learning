eb7e4f7ab7d039e615f814da689a9828d148152d
shweta <121001392+shwetaTh@users.noreply.github.com>
2024-07-06 15:44:46 +0545

    modified for lab 3 to see content type and modified date

--- NetworkProgramming/LabWorks/Unit5/Lab123.java
+++ NetworkProgramming/LabWorks/Unit5/Lab123.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 import java.net.URL;
 import java.net.URLConnection;
 
-public class Lab1 {
+public class Lab123 {
     public static void main(String[] args) {
         try {
             // Establish a URLConnection to the specified URL
