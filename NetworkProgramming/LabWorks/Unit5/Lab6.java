78589d59ad5c529e7d183ef8746329280dd51332
shweta <121001392+shwetaTh@users.noreply.github.com>
2024-07-06 16:26:39 +0545

    http response handling code

--- NetworkProgramming/LabWorks/Unit5/Lab6.java
+++ NetworkProgramming/LabWorks/Unit5/Lab6.java
@@ -4,31 +4,33 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class Lab6 {
-    public static String handleResponse(HttpURLConnection connection) throws IOException {
-        int responseCode = connection.getResponseCode();
-        String responseMessage = connection.getResponseMessage();
-        StringBuilder response = new StringBuilder();
-        response.append("Response Code: ").append(responseCode).append("\n");
-        response.append("Message: ").append(responseMessage).append("\n");
-        if (responseCode == HttpURLConnection.HTTP_OK) {
-            try (BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()))) {
-                String inputLine;
-                StringBuilder content = new StringBuilder();
-                while ((inputLine = in.readLine()) != null) {
-                    content.append(inputLine+ "\n");
-                }
-                response.append("Content: ").append(content.toString());
-            }
-        }
-        return response.toString();
-    }
     public static void main(String[] args) {
         try {
             String getUrl = "https://jsonplaceholder.typicode.com/posts/1";
-            HttpURLConnection connection = Lab5.sendGetRequest(getUrl);
-            String response = handleResponse(connection);
-            System.out.println(response);
+            HttpURLConnection connection = (HttpURLConnection) new URL(getUrl).openConnection();
+            connection.setRequestMethod("GET");
+
+            StringBuilder response = new StringBuilder();
+            int responseCode = connection.getResponseCode();
+            String responseMessage = connection.getResponseMessage();
+
+            response.append("Response Code: ").append(responseCode).append("\n");
+            response.append("Message: ").append(responseMessage).append("\n");
+
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                try (BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()))) {
+                    String inputLine;
+                    while ((inputLine = in.readLine()) != null) {
+                        response.append(inputLine).append("\n");
+                    }
+                }
+            }
+
+            System.out.println(response.toString());
+
         } catch (IOException e) {
             e.printStackTrace();
         }
