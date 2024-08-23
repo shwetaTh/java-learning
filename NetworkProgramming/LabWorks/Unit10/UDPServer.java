80543ab2d795ee21aa7ee1ec93aaf2e5164add58
shweta <121001392+shwetaTh@users.noreply.github.com>
2024-08-23 14:34:57 +0545

    UDP server side

---
+++ NetworkProgramming/LabWorks/Unit10/UDPServer.java
@@ -0,0 +1,35 @@
+package LabWorks.Unit10;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
+public class UDPServer {
+    public static void main(String[] args) {
+        int port = 9876;
+
+        try (DatagramSocket serverSocket = new DatagramSocket(port)) {
+            byte[] receiveBuffer = new byte[1024];
+
+            System.out.println("Server is listening on port " + port);
+
+            while (true) {
+                DatagramPacket receivePacket = new DatagramPacket(receiveBuffer, receiveBuffer.length);
+                serverSocket.receive(receivePacket);
+
+                String clientMessage = new String(receivePacket.getData(), 0, receivePacket.getLength());
+                System.out.println("Received from client: " + clientMessage);
+
+                String responseMessage = "Echo: " + clientMessage;
+                byte[] sendBuffer = responseMessage.getBytes();
+
+                InetAddress clientAddress = receivePacket.getAddress();
+                int clientPort = receivePacket.getPort();
+
+                DatagramPacket sendPacket = new DatagramPacket(sendBuffer, sendBuffer.length, clientAddress, clientPort);
+                serverSocket.send(sendPacket);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
